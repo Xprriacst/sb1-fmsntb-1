7d5410c3cd505c5866c0c5db498cca7e04d0a92c
Xprriacst <expertiaen5min@gmail.com>
2024-11-28 17:03:52 +0100

    Add files via upload

---
+++ App.svelte
@@ -0,0 +1,10 @@
+<content>
+<script lang="ts">
+  import { Frame, Page } from '@nativescript/core';
+  import Home from './components/Home.svelte';
+</script>
+
+<frame>
+  <Home />
+</frame>
+</content>
